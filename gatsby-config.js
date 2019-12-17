72db7476bcdf0d6164070314cff12fc446097f84
esportleague <58951999+esportleague@users.noreply.github.com>
2019-12-17 21:16:25 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
+        name: `minimal-blog - @lekoarts/gatsby-theme-cara`,
         short_name: `minimal-blog`,
         description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
         start_url: `/`,
